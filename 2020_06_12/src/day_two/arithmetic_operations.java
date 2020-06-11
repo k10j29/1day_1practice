bd3291d980ab34c55c451770754ae6acd6de4488
unknown <k10j29@naver.com>
2020-06-12 00:57:20 +0900

    initial commit

---
+++ 2020_06_12/src/day_two/arithmetic_operations.java
@@ -0,0 +1,22 @@
+package day_two;
+
+import java.util.Scanner;
+
+public class arithmetic_operations {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+
+	}
+
+}
