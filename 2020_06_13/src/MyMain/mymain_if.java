fab42e169cad6c0d894cb19a3a5816a88533b52d
unknown <k10j29@naver.com>
2020-06-13 00:10:37 +0900

    initial commit

---
+++ 2020_06_13/src/MyMain/mymain_if.java
@@ -0,0 +1,24 @@
+package MyMain;
+
+import java.util.Scanner;
+
+public class mymain_if {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+
+		if (A > B) {
+			System.out.println(">");
+		} else if (A < B) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+
+		}
+	}
+
+}
