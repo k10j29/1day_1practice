8ea1ab60da5d396a11a10063cc5f8eb718ee2d9d
unknown <k10j29@naver.com>
2020-06-16 23:47:40 +0900

    initial commit

---
+++ 2020_06_16/src/MyMain/mymain_for.java
@@ -0,0 +1,32 @@
+package MyMain;
+
+import java.util.Scanner;
+
+public class mymain_for {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i < 10; i++) {
+			// &&&&& 부등호 사용시 띄어쓰기
+			System.out.println(n + " * " + i + " = " + (n * i));
+
+		}
+
+		sc.close();
+
+	}
+
+}
+
+//문제
+//N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
+//
+//입력
+//첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
+//
+//출력
+//출력형식과 같게 N*1부터 N*9까지 출력한다.
