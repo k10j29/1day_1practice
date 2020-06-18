24fb4de9a785030e7dd20128dbcb2ada539bb389
unknown <k10j29@naver.com>
2020-06-19 00:01:25 +0900

    initial commit

---
+++ 2020_06_18/src/MyMain/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ 
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+    if(a > 100000) {
+      System.exit(0);
+    }
+    for(int i=1; i<=a; i++) {
+      System.out.println(i);
+    }
+  }//end of main
+}
